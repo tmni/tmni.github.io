7a1e5887020083525b7be7f380e0d60d39ce94bd
Taruna Emani <temani@andrew.cmu.edu>
2019-09-30 11:37:58 -0400

    questions

--- jsQuiz/question.js
+++ jsQuiz/question.js
@@ -4,9 +4,10 @@ class Question {
     this.choices = choices;
     this.answer = answer;
 }
-}
 var questions = [
 new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
 new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
 new Question("What has not been covered in PUI lab?", ["HTML", "CSS","Javascript", "Java"], "Java"),
 ];
+
+}

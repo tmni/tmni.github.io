e2f9886c39fa705c34ad0597bc61936be563498f
Taruna Emani <temani@andrew.cmu.edu>
2019-10-28 01:37:58 -0400

    fixed storage of items

--- assign6B/main.js
+++ assign6B/main.js
@@ -21,6 +21,29 @@ class CartItem{
      return this.fluff}
    }
 
+
+   function populateCart(){
+     var cartItems = JSON.parse(localStorage.getItem("cart"));
+     table = document.getElementById("cart-table");
+     console.log(cartItems)
+     for(var i = 0; i < cartItems.length; i++)
+          {
+              // create a new row
+              var newRow = table.insertRow(table.length);
+              for(var j = 0; j < cartItems[i].length; j++)
+              {
+                  // create a new cell
+                  var cell = newRow.insertCell(j);
+
+                  // add value to the cell
+                  cell.innerHTML = cartItems[i][j];
+              }
+          }
+
+   }
+
+
+
    function addToCart(){
      var amount = document.getElementById("amt").value;
      if (amount >0){
@@ -39,22 +62,19 @@ class CartItem{
 
        var total = parseInt(prev) + parseInt(amount)
        var item = new CartItem(currentItem["name"], currentItem["color"], currentItem["material"])
-       console.log("looping ",amount, " times")
+       // console.log("looping ",amount, " times")
        for (i = 0; i< amount; i++) {
-         console.log("cart item ", i)
+         // console.log("cart item ", i)
          cart.push(item)
        }
-         console.log("here's the cart", cart)
+         // console.log("here's the cart", cart)
 
        document.getElementById("cart").innerHTML = "Cart (" + String(total) + ")"
        alert("Added " + amount + " items to cart!")
-       localStorage.setItem("cartTot", JSON.stringify(total));
+       localStorage.setItem("cartTot", JSON.stringify(cart.length));
+       localStorage.setItem("cart", JSON.stringify(cart));
+
        // console.log("the cart now has, ", cart)
-
-
-
-
-
 
      }
        }
@@ -62,7 +82,10 @@ class CartItem{
       alert("Make sure you choose a color, material, and at least one pillow")
     }
      }
-   function ProductTrack(){
+
+
+
+   function productTrack(){
      currentItem["name"] = document.getElementById("pillow-title").innerHTML.trim()
      var cartnum = JSON.parse(localStorage.getItem("cartTot"))
      console.log("the cart num is" + cartnum)
@@ -75,8 +98,22 @@ class CartItem{
      $(".dot").removeClass("selected");
      // console.log("changed color", currentItem)
      $(this).addClass("selected");
-     currentItem["color"] = this
-     // console.log("new color", currentItem)
+     switch(this.getAttribute("class")) {
+        case "dot red selected":
+          currentItem["color"] = "After School Special";
+          break;
+        case "dot yellow selected":
+          currentItem["color"] = "Morning Hazel";
+          break;
+        case "dot blue selected":
+          currentItem["color"] = "Cozy Denim";
+          break;
+          case "dot green selected":
+            currentItem["color"] = "Rainy Day";
+            break;
+          }
+
+     console.log("new color", currentItem)
 
      });
      });
@@ -86,7 +123,7 @@ class CartItem{
      $(".material").removeClass("sel");
      // console.log("changed material", currentItem)
      $(this).addClass("sel");
-     currentItem["material"] = this
+     currentItem["material"] = this.alt
      // console.log("added material", currentItem)
      });
      });
